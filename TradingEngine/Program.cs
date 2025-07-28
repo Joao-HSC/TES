f469905809dfd7a055591fc4eb4d9b9cf8d5fa9c
Joao-HSC <joaocabaco2004@gmail.com>
2025-07-28 04:27:59 -0300

    Oopsie

---
+++ TradingEngine/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using TradingEngineServer.Core;
+
---
+++ TradingEngine/TradingEngine.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Runtime.InteropServices;
+using TradingEngineServer.Core.Configuration;
+
+namespace TradingEngineServer.Core
+{
+    internal class TradingEngineServer : BackgroundService, ITradingEngineServer
+    {
+        private readonly ILogger<TradingEngineServer> _logger;
+        private readonly TradingEngineServerConfiguration _tradingEngineServerConfig;
+        public TradingEngineServer(ILogger<TradingEngineServer> logger, IOptions<TradingEngineServerConfiguration> config)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _tradingEngineServerConfig = config.Value ?? throw new ArgumentNullException(nameof(config));
+        }
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+            }
+        }
+    }
+}
